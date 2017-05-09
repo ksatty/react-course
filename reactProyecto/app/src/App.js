8015dc4135d4be8c20b1939689747d436a48a8c8
Horacio Casatti <horacio.casatti@mercadolibre.com>
2017-05-09 19:37:03 -0300

    first commit

---
+++ reactProyecto/app/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hola from './components/Hola';
+
+ReactDOM.render(
+	<Hola />,
+	
+	document.getElementById('root')
+);
---
+++ reactProyecto/app/src/components/Hola.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Hola extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<h1>Hola mundo</h1>
+		);
+	}
+};
